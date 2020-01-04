59f48eaf890a3275d22d4182d838c6c07570d3fd
piotrek19 <dziobap@gmail.com>
2020-01-04 20:33:33 +0100

    GET visits endpoint invocation added

--- web-app-web/src/main/java/net/dzioba/petclinicmicro/petclinicmicrowebapp/api/v1/service/VisitServiceClientSimulator.java
+++ web-app-web/src/main/java/net/dzioba/petclinicmicro/petclinicmicrowebapp/api/v1/service/VisitServiceClientSimulator.java
@@ -1,11 +1,8 @@
 package net.dzioba.petclinicmicro.petclinicmicrowebapp.api.v1.service;
 
-import net.dzioba.petclinicmicro.common.model.PossibleVisitListDTO;
-import net.dzioba.petclinicmicro.common.model.VisitDTO;
-
 public interface VisitServiceClientSimulator {
 
-    PossibleVisitListDTO findPossibleVisitsForDate();
+    void findPossibleVisitsForDate();
 
-    VisitDTO scheduleThisVisit();
+    void scheduleThisVisit();
 }
--- web-app-web/src/main/java/net/dzioba/petclinicmicro/petclinicmicrowebapp/api/v1/service/scheduled/VisitServiceClientSimulatorScheduled.java
+++ web-app-web/src/main/java/net/dzioba/petclinicmicro/petclinicmicrowebapp/api/v1/service/scheduled/VisitServiceClientSimulatorScheduled.java
@@ -1,38 +1,65 @@
 package net.dzioba.petclinicmicro.petclinicmicrowebapp.api.v1.service.scheduled;
 
-import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import net.dzioba.petclinicmicro.common.model.PossibleVisitListDTO;
-import net.dzioba.petclinicmicro.common.model.VisitDTO;
 import net.dzioba.petclinicmicro.petclinicmicrowebapp.api.v1.service.VisitServiceClientSimulator;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
-@RequiredArgsConstructor
+import java.time.LocalDate;
+import java.util.Random;
+
 @Slf4j
+@Setter
+@ConfigurationProperties(prefix = "net.dzioba.petclinicmicro")
 @Service
 public class VisitServiceClientSimulatorScheduled implements VisitServiceClientSimulator {
 
+    private final Integer maxDayOffset = 11;
 
-    //private final RestTemplate restTemplate;
+    private String clinicManagerAppHost;
+    private String clinicManagerApiV1Visits;
 
-    @Transactional
-    @Scheduled(fixedRate = 3000) //run every 3 seconds
-    @Override
-    public PossibleVisitListDTO findPossibleVisitsForDate() {
-        log.debug("Scheduled method findPossibleVisitsForDate started");
+    private final RestTemplate restTemplate;
 
-        return null;
+    public VisitServiceClientSimulatorScheduled(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
     }
 
     @Transactional
     @Scheduled(fixedRate = 3000) //run every 3 seconds
     @Override
-    public VisitDTO scheduleThisVisit() {
+    public void findPossibleVisitsForDate() {
+        log.debug("Scheduled method findPossibleVisitsForDate started");
+
+
+        String visitDate = LocalDate.now().plusDays(new Random().nextInt(maxDayOffset)).toString();
+
+        String url = UriComponentsBuilder.fromUriString(clinicManagerAppHost + clinicManagerApiV1Visits)
+                .queryParam("visitDate", visitDate).toUriString();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
+        log.debug("Sending GET request to: " + url);
+        ResponseEntity<PossibleVisitListDTO> responseEntity = restTemplate.exchange(url, HttpMethod.GET, new HttpEntity<>(headers), ParameterizedTypeReference.forType(PossibleVisitListDTO.class));
+        log.debug("Response received: \n" + responseEntity.getBody());
+    }
+
+    @Transactional
+    @Scheduled(fixedRate = 3000) //run every 3 seconds
+    @Override
+    public void scheduleThisVisit() {
         log.debug("Scheduled method scheduleThisVisit started");
 
-        return null;
     }
 
 }
+
